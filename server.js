4fbcfaf0d427c3ffb36c2da8a7b53c9420d45ffa
Ravi <RaviOP@users.noreply.github.com>
2020-10-21 15:58:13 +0530

    =Made Home Page

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 const viewsPath = path.join(__dirname, 'resources/views')
+const publicPath = path.join(__dirname, 'public')
 
 app.set('view engine', 'ejs')
 app.set('views', viewsPath)
-app.use(expressLayout)
+app.use(express.static(publicPath))
+// app.use(expressLayout)
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
 app.listen(PORT, () => {
     console.log(`Server is Up and Running On Port ${PORT}`)
